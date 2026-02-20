10ba35165e12958651022da4d15dbc3c865e40c3
robng <49914302+robng@users.noreply.github.com>
2026-02-20 20:44:52 +0100

    chore: set tab size based on file type

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,1 +1,17 @@
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "javascript", "typescript", "html", "css", "json", "xml" },
+  callback = function()
+    vim.opt_local.tabstop = 2
+    vim.opt_local.shiftwidth = 2
+    vim.opt_local.expandtab = true
+  end,
+})
 
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "cs" },
+  callback = function()
+    vim.opt_local.tabstop = 4
+    vim.opt_local.shiftwidth = 4
+    vim.opt_local.expandtab = true
+  end,
+})
