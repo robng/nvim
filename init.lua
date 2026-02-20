7f38b9652ffb2bc522ab0e45c9da7ab891965119
robng <49914302+robng@users.noreply.github.com>
2026-02-21 00:37:51 +0100

    chore: add themes, prefix plugins/*.lua files by purpose

--- init.lua
+++ init.lua
@@ -1,2 +1,4 @@
--- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+vim.cmd("colo everforest")
+vim.cmd("set cc=81")
---
+++ lua/plugins/theme__everforest.lua
@@ -0,0 +1,7 @@
+return {
+  "sainnhe/everforest",
+  config = function()
+    vim.g.everforest_background = "soft"
+    vim.g.everforest_enable_italic = true
+  end,
+}
