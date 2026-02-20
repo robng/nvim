d3a43608ccff9619e3be5386f3674a069aaa2ab0
robng <49914302+robng@users.noreply.github.com>
2026-02-20 22:59:23 +0100

    chore: update, improve keymap

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,6 +1,11 @@
+vim.keymap.del("n", "<C-h>")
+
+vim.keymap.set("n", "<C-e>", "<leader>e")
+vim.keymap.set("n", "<C-#>", "gcc")
+vim.keymap.set({ "n", "v" }, "J", "Lzz")
+vim.keymap.set({ "n", "v" }, "K", "Hzz")
+vim.keymap.set("t", "<esc>", [[<C-\><C-n>]])
+
 vim.keymap.set("n", ",", ";", { noremap = true })
 vim.keymap.set("n", ";", ",", { noremap = true })
-vim.keymap.set({ "n", "v" }, "<S-J>", "Lzz", { noremap = true })
-vim.keymap.set({ "n", "v" }, "<S-K>", "Hzz", { noremap = true })
-vim.keymap.set("t", "<Esc>", [[<C-\><C-n>]])
 vim.keymap.set("n", "ä", "@")
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -5,7 +5,28 @@ return {
       servers = {
         ["*"] = {
           keys = {
-            { "K", false },
+            -- Disable LazyVim defaults
+            { "K", enabled = false },
+
+            -- Set up custom keys
+            {
+              "<C-.>",
+              function()
+                return vim.lsp.buf.code_action()
+              end,
+            },
+            {
+              "<C-h>",
+              function()
+                return vim.lsp.buf.hover()
+              end,
+            },
+            {
+              "<C-r>",
+              function()
+                return vim.lsp.buf.rename()
+              end,
+            },
           },
         },
       },
