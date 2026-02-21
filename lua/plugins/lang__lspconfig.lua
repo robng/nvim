455bd2987487a43b90b9dfb43fb1bda3b9f8d41d
robng <49914302+robng@users.noreply.github.com>
2026-02-21 11:15:52 +0100

    chore: rename lsp.lua for consistency

---
+++ lua/plugins/lang__lspconfig.lua
@@ -0,0 +1,43 @@
+return {
+  "neovim/nvim-lspconfig",
+  opts = {
+    servers = {
+      ["*"] = {
+
+        -- -------------------------------------------------------------
+        --  Customize keys
+        -- -------------------------------------------------------------
+
+        keys = {
+
+          -- Disable defaults
+
+          { "K", enabled = false },
+          { "<C-k>", enabled = false },
+
+          -- Define custom keys
+
+          {
+            "<C-.>",
+            function()
+              return vim.lsp.buf.code_action()
+            end,
+          },
+          -- TODO: <C-e>: show [e]rrors (aka diagnostics)
+          {
+            "<C-h>",
+            function()
+              return vim.lsp.buf.hover()
+            end,
+          },
+          {
+            "<C-r>",
+            function()
+              return vim.lsp.buf.rename()
+            end,
+          },
+        },
+      },
+    },
+  },
+}
