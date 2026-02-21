ecfeeafa42bdbe71bdb9b17b2a50dab4c6b49259
robng <49914302+robng@users.noreply.github.com>
2026-02-21 11:26:49 +0100

    chore: customize blink.cmp

---
+++ lua/plugins/other__blink.lua
@@ -0,0 +1,30 @@
+return {
+  "saghen/blink.cmp",
+  opts = {
+
+    -- -----------------------------------------------------------------
+    --  Customize keys
+    -- -----------------------------------------------------------------
+
+    keymap = {
+      preset = "default",
+      ["<C-j>"] = { "select_next", "fallback" },
+      ["<C-k>"] = { "select_prev", "fallback" },
+      ["<Tab>"] = { "accept", "fallback" },
+      ["<Cr>"] = { "accept", "fallback" },
+    },
+
+    -- -----------------------------------------------------------------
+    --  Disable snippets
+    -- -----------------------------------------------------------------
+
+    -- sources = {
+    --   transform_items = function(_, items)
+    --     return vim.tbl_filter(function(item)
+    --       return item.kind
+    --         ~= require("blink.cmp.types").CompletionItemKind.Snippet
+    --     end, items)
+    --   end,
+    -- },
+  },
+}
