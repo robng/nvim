4846421c662dfb3a91fd18e5de22706c0621fc9b
robng <49914302+robng@users.noreply.github.com>
2026-02-23 01:28:15 +0100

    chore: disable ghost text, change theme, unmark `vim` as global

--- init.lua
+++ init.lua
@@ -1,6 +1,6 @@
 require("config.lazy")
 
-vim.cmd("colo everforest")
+vim.cmd("colo gruvbox-material")
 vim.cmd("set cc=81")
 
 vim.api.nvim_set_hl(0, "SnacksPickerGitStatusUntracked", { link = "Special" })
