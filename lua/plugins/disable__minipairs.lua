a6486242d9ba5a164bdaf2cb10c760748e7ce938
robng <49914302+robng@users.noreply.github.com>
2026-02-26 21:34:21 +0100

    feat: disable minipairs

---
+++ lua/plugins/disable__minipairs.lua
@@ -0,0 +1,10 @@
+return {
+  "nvim-mini/mini.pairs",
+  enabled = false,
+
+  -- opts = function(_)
+  --   -- Disable inserting an extra newline after cursor if
+  --   -- enter is pressed between the opening closing symbols
+  --   vim.keymap.set("i", "<CR>", "<CR>", { buffer = false })
+  -- end,
+}
