d91c800ea69bcb80eb50cada74009802854765fe
robng <49914302+robng@users.noreply.github.com>
2026-03-25 17:54:35 +0100

    chore: disable snippets (& combine all disabled plugins into one file)

---
+++ lua/plugins/disabled.lua
@@ -0,0 +1,6 @@
+return {
+  { "akinsho/bufferline.nvim",      enabled = false },
+  { "folke/flash.nvim",             enabled = false },
+  { "nvim-mini/mini.pairs",         enabled = false },
+  { "rafamadriz/friendly-snippets", enabled = false },
+}
